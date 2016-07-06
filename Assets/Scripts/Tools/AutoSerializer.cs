06a6f54cd0fdbbd43b2527b745779a3f1be960c7
Chengxue Lee <Chengxue Lee>
2016-07-07 07:55:25 +0800

    存檔和聲音

---
+++ Assets/Scripts/Tools/AutoSerializer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+
+public static class AutoSerializer {
+    public static void Save<T>(object obj, string path)
+    {
+        CreateXML(SerializeObject<T>(obj), path);
+    }
+    public static T Load<T>(string path, Type type)
+    {
+        string data = LoadXML(path);
+        if (data == null)
+        {
+            return default(T);
+        }
+        return (DeserializeObject<T>(data, type));
+    }
+
+    public static string SerializeObject<T>(object pObject)
+    {
+        string XmlizedString = null;
+        MemoryStream memoryStream = new MemoryStream();
+        XmlSerializer xs = new XmlSerializer(typeof(T));//需要更改類別
+        XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+        xs.Serialize(xmlTextWriter, pObject);
+        memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+        XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray());
+        return XmlizedString;
+    }
+    public static T DeserializeObject<T>(string pXmlizedString, Type type)
+    {
+        XmlSerializer xs;
+        MemoryStream memoryStream;
+        object o;
+        try
+        {
+            xs = new XmlSerializer(type);//需要更改類別
+            memoryStream = new MemoryStream(StringToUTF8ByteArray(pXmlizedString));
+            o = xs.Deserialize(memoryStream);
+        }
+        catch { return default(T); }
+        // XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+        return (T)o;
+    }
+
+    //-----------------------------基礎內部function
+    private static string UTF8ByteArrayToString(byte[] characters)
+    {
+        UTF8Encoding encoding = new UTF8Encoding();
+        string constructedString = encoding.GetString(characters);
+        return (constructedString);
+    }
+    private static byte[] StringToUTF8ByteArray(string pXmlString)
+    {
+        UTF8Encoding encoding = new UTF8Encoding();
+        byte[] byteArray = encoding.GetBytes(pXmlString);
+        return byteArray;
+    }
+    private static void CreateXML(string data, string fileLocation)
+    {
+        StreamWriter writer;
+        FileInfo t = new FileInfo(fileLocation);
+        if (!t.Exists)
+        {
+            writer = t.CreateText();
+        }
+        else
+        {
+            t.Delete();
+            writer = t.CreateText();
+        }
+        writer.Write(data);
+        writer.Close();
+
+    }
+    private static string LoadXML(string fileLocation)
+    {
+        if (File.Exists(fileLocation))
+        {
+            StreamReader r = File.OpenText(fileLocation);
+            string info = r.ReadToEnd();
+            r.Close();
+
+            return info;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Tools/SoundManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+    private static SoundManager instance;
+    private AudioSource bgm;
+    private AudioSource sfx1, sfx2;
+    private static IEnumerator tween = null;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+        bgm = this.gameObject.AddComponent<AudioSource>();
+        sfx1 = this.gameObject.AddComponent<AudioSource>();
+        sfx2 = this.gameObject.AddComponent<AudioSource>();
+        bgm.loop = true;
+        sfx1.loop = sfx2.loop = false;
+    }
+	public static void PlayBGM(AudioClip clip)
+    {
+        instance.bgm.clip = clip;
+        instance.bgm.Play();
+    }
+    public static void PlaySfx1(AudioClip clip) {
+        instance.sfx1.clip = clip;
+        instance.sfx1.Play();
+    }
+    public static void PlaySfx2(AudioClip clip)
+    {
+        instance.sfx2.clip = clip;
+        instance.sfx2.Play();
+    }
+    public static void TweenVolume(float from, float to, float time, Action onFinish)
+    {
+        if (tween != null)
+            instance.StopCoroutine(tween);
+        tween = VolumeChange(from, to, time, onFinish);
+        instance.StartCoroutine(tween);
+    }
+    private static IEnumerator VolumeChange(float from, float to, float time, Action onFinish)
+    {
+        float timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            instance.bgm.volume = Mathf.Lerp(from, to, timer / time);
+            yield return null;
+        }
+        onFinish();
+        tween = null;
+    }
+}
