49f0776838c2e0fcb7edef941c64969be258c417
st9007a <st9007a@gmail.com>
2016-07-11 21:17:56 +0800

    基本怪物架構

---
+++ Assets/Scripts/RoleControlAndMonsterAI/Class/Monster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster {
+
+    private int hp;
+    private int baseHp;
+
+    private float speed;
+    private float baseSpeed;
+
+    private float attack;
+    private float baseAttack;
+
+    public int Hp {
+        set { hp = value; }
+        get { return hp; }
+    }
+    public int BaseHp {
+        set { baseHp = value; }
+        get { return baseHp; }
+    }
+
+    public float Speed {
+        set { speed = value; }
+        get { return speed; }
+    }
+    public float BaseSpeed {
+        set { baseSpeed = value; }
+        get { return baseSpeed; }
+    }
+
+    public float Attack {
+        set { attack = value; }
+        get { return attack; }
+    }
+    public float BaseAttack {
+        set { baseAttack = value; }
+        get { return baseAttack; }
+    }
+}
