b935b8244a14397e601611af4ea0e9166344221e
cyshenfrog <cyshenfrog@gmail.com>
2016-07-30 12:09:27 +0800

    Time&Clock

---
+++ Assets/Scripts/WorldEventControl/Timer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    
+    public float realSecond { get; protected set; }
+    void Update ()
+    {
+        realSecond = Time.timeSinceLevelLoad;
+	}
+
+}
