70252de7f15796f65c7190b72eedc07050c2b974
Chengxue Li <930leo@gmail.com>
2016-08-20 14:26:10 +0800

    Clock GUI and show role image

---
+++ Assets/Scripts/GUI/ClockGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockGUI : MonoBehaviour {
+    public GameObject niddle;
+    public float from, to;
+	public float Time
+    {
+        set
+        {
+            if (value > 1 || value < 0)
+            {
+                Debug.LogError("The Clock time value is out of 0 ~ 1. The value will be ignored.");
+                return;
+            }
+            Vector3 angle = new Vector3(0, 0, Mathf.Lerp(from, to, value));
+            niddle.transform.localEulerAngles = angle;
+        }
+    }
+    public void Reset()
+    {
+        Time = 0f;
+    }
+}
