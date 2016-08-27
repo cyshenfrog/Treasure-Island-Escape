276e7c68cf5456f34a8e38365c6b719db0164c79
Chengxue Li <930leo@gmail.com>
2016-08-27 12:05:36 +0800

    因為 Assets/Editor 被 ignore了，所以我把Editor移到Assets/Scripts/GUI/底下

---
+++ Assets/Scripts/GUI/Editor/HorizontalLayoutGroupResizerEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(HorizontalLayoutGroupResizer))]
+public class HorizontalLayoutGroupResizerEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Resize"))
+            ((HorizontalLayoutGroupResizer)target).Resize();
+    }
+}
---
+++ Assets/Scripts/GUI/Editor/VerticalLayoutGroupResizerEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(VerticalLayoutGroupResizer))]
+public class VerticalLayoutGroupResizerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Resize"))
+            ((VerticalLayoutGroupResizer)target).Resize();
+    }
+}
