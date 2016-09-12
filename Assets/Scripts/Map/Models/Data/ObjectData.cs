4a054020bf25a0cc7c4147ddb081c9a2fce64caf
CaramelYo <alltime326@gmail.com>
2016-09-12 11:33:09 +0800

    New the Object Struct 	new file:   Assets/Scripts/Map/Models/Data.meta 	new file:   Assets/Scripts/Map/Models/Data/BuildingData.cs 	new file:   Assets/Scripts/Map/Models/Data/BuildingData.cs.meta 	new file:   Assets/Scripts/Map/Models/Data/ObjectData.cs 	new file:   Assets/Scripts/Map/Models/Data/ObjectData.cs.meta 	new file:   Assets/Scripts/Map/Models/Data/ResourceData.cs 	new file:   Assets/Scripts/Map/Models/Data/ResourceData.cs.meta 	new file:   Assets/Scripts/Map/Models/Data/TerrainFeatureData.cs 	new file:   Assets/Scripts/Map/Models/Data/TerrainFeatureData.cs.meta 	new file:   Assets/Scripts/Map/Models/Data/TileData.meta 	renamed:    Assets/Scripts/Map/Models/Desert.cs -> Assets/Scripts/Map/Models/Data/TileData/Desert.cs 	renamed:    Assets/Scripts/Map/Models/Desert.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Desert.cs.meta 	renamed:    Assets/Scripts/Map/Models/Forest.cs -> Assets/Scripts/Map/Models/Data/TileData/Forest.cs 	renamed:    Assets/Scripts/Map/Models/Forest.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Forest.cs.meta 	renamed:    Assets/Scripts/Map/Models/Grassland.cs -> Assets/Scripts/Map/Models/Data/TileData/Grassland.cs 	renamed:    Assets/Scripts/Map/Models/Grassland.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Grassland.cs.meta 	renamed:    Assets/Scripts/Map/Models/Marsh.cs -> Assets/Scripts/Map/Models/Data/TileData/Marsh.cs 	renamed:    Assets/Scripts/Map/Models/Marsh.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Marsh.cs.meta 	renamed:    Assets/Scripts/Map/Models/Sea.cs -> Assets/Scripts/Map/Models/Data/TileData/Sea.cs 	renamed:    Assets/Scripts/Map/Models/Sea.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Sea.cs.meta 	renamed:    Assets/Scripts/Map/Models/Snowfield.cs -> Assets/Scripts/Map/Models/Data/TileData/Snowfield.cs 	renamed:    Assets/Scripts/Map/Models/Snowfield.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Snowfield.cs.meta 	renamed:    Assets/Scripts/Map/Models/TileData.cs -> Assets/Scripts/Map/Models/Data/TileData/TileData.cs 	renamed:    Assets/Scripts/Map/Models/TileData.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/TileData.cs.meta 	renamed:    Assets/Scripts/Map/Models/Volcano.cs -> Assets/Scripts/Map/Models/Data/TileData/Volcano.cs 	renamed:    Assets/Scripts/Map/Models/Volcano.cs.meta -> Assets/Scripts/Map/Models/Data/TileData/Volcano.cs.meta 	new file:   Assets/Scripts/Map/Models/Display.meta 	new file:   Assets/Scripts/Map/Models/Display/BuildingDisplay.cs 	new file:   Assets/Scripts/Map/Models/Display/BuildingDisplay.cs.meta 	new file:   Assets/Scripts/Map/Models/Display/ObjectDisplay.cs 	new file:   Assets/Scripts/Map/Models/Display/ObjectDisplay.cs.meta 	new file:   Assets/Scripts/Map/Models/Display/ResourceDisplay.cs 	new file:   Assets/Scripts/Map/Models/Display/ResourceDisplay.cs.meta 	new file:   Assets/Scripts/Map/Models/Display/TerrainFeatureDisplay.cs 	new file:   Assets/Scripts/Map/Models/Display/TerrainFeatureDisplay.cs.meta 	new file:   Assets/Scripts/Map/Models/Display/TileDisplay.cs 	new file:   Assets/Scripts/Map/Models/Display/TileDisplay.cs.meta 	new file:   Assets/Scripts/Map/Models/Form.meta 	renamed:    Assets/Scripts/Map/Models/Ellipse.cs -> Assets/Scripts/Map/Models/Form/Ellipse.cs 	renamed:    Assets/Scripts/Map/Models/Ellipse.cs.meta -> Assets/Scripts/Map/Models/Form/Ellipse.cs.meta 	renamed:    Assets/Scripts/Map/Models/Form.cs -> Assets/Scripts/Map/Models/Form/Form.cs 	renamed:    Assets/Scripts/Map/Models/Form.cs.meta -> Assets/Scripts/Map/Models/Form/Form.cs.meta

---
+++ Assets/Scripts/Map/Models/Data/ObjectData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class ObjData
+{
+    public static ObjData Create(int type0 = 0, int type1 = 0, int type2 = 0, int type3 = 0)
+    {
+        switch(type0)
+        {
+            //...
+            default: return null;
+        }
+    }
+
+    //this is used to create sub obj under a main(center) obj
+    public static ObjData SubCreate(int type0 = 0, int type1 = 0, int type2 = 0, int type3 = 0)
+    {
+        switch (type0)
+        {
+            //...
+            default: return null;
+        }
+    }
+
+    //bodies are localPosition
+    public List<ObjData> Bodies
+    {
+        get;
+        private set;
+    }
+
+    public Vector3 Center
+    {
+        get { return center; }
+        set { center = value; }
+    }
+
+    public int State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public int No
+    {
+        get { return no; }
+        set { no = value; }
+    }
+
+    //position and center are world positions
+    protected Vector3 position, center;
+    protected Sprite sprite;
+    protected int no, state, type0, type1, type2, type3;
+}
---
+++ Assets/Scripts/Map/Models/Display/ObjectDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class ObjDisplay : MonoBehaviour
+{
+    public abstract void Idle(int mode);
+    public abstract void Click();
+    public abstract void Collision(Transform collision);
+
+    protected ObjData data;
+    protected Action<Transform> CollisionAction;
+    protected Action<int> IdleAction;
+    protected Action ClickAction;
+}
