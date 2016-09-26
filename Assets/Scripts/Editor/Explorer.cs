d55e9a79df9184595db2c3882c7b03fef0849b04
Chengxue Lee <Chengxue Lee>
2016-09-26 18:48:01 +0800

    能夠從menuitem開啟explorer

---
+++ Assets/Scripts/Editor/Explorer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System.Collections;
+
+public static class Explorer {
+    [MenuItem("Explorer/Persistent Data Path")]
+    public static void OpenPersistentDataPath()
+    {
+        EditorUtility.RevealInFinder(Application.persistentDataPath);
+    }
+    [MenuItem("Explorer/Data Path")]
+    public static void OpenDataPath()
+    {
+        EditorUtility.RevealInFinder(Application.dataPath);
+    }
+}
