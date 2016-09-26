30fab62748eb00f68267f2d2d542c764d9a578e0
uno <st9007a@gmail.com>
2016-09-27 00:22:55 +0800

    build polygon path

--- Assets/Scripts/Monster/MonsterController/Controller/MController_8.cs
+++ Assets/Scripts/Monster/MonsterController/Controller/MController_8.cs
@@ -7,39 +7,69 @@ public class MController_8 : MController_2 {
     //攻城獅王
 
     //path
-    public int CornerCount;
-    public int SideLength = 10;
+    public int VertexCount;
+    public int SideRange = 10;
     public Vector2 Center = new Vector2(0, 0);
 
-    private List<Vector2> corner = new List<Vector2>();
+    private List<Vector2> vertex = new List<Vector2>();
     private int targetCornor = 0;
 
     protected override void Awake() {
         base.Awake();
 
-        for (int i = 0; i < CornerCount; i++) {
-            int x = Random.Range(-5, 5);
-            int y = Random.Range(-5, 5);
-            corner.Add(new Vector2(x, y));
+        for (int i = 0; i < VertexCount; i++) {
+            int x = Random.Range(-SideRange/2, SideRange/2);
+            int y = Random.Range(-SideRange/2, SideRange/2);
+            vertex.Add(new Vector2(x, y));
         }
+
+        sortVertex();
     }
 
-    public override void Idle() {
-        Move();
+    protected override void Start() {
+        State = MonsterState.MOVE;
+    }
+
+    protected override void Update() {
+        stateMachine();
+    }
+
+    /// <summary>
+    /// sort vertex to polygon path
+    /// </summary>
+    private void sortVertex() {
+        List<Vector2> q1q2 = new List<Vector2>();
+        List<Vector2> q3q4 = new List<Vector2>();
+
+        int c = vertex.Count;
+        for (int i = 0; i < c; i++) {
+            if (vertex[i].y >= 0) q1q2.Add(vertex[i]);
+            else q3q4.Add(vertex[i]);
+        }
+
+        q1q2.Sort((x, y) => { return -x.x.CompareTo(y.x); });
+        q3q4.Sort((x, y) => { return x.x.CompareTo(y.x); });
+
+        vertex = q1q2;
+
+        c = q3q4.Count;
+        for (int i = 0; i < c; i++)
+            vertex.Add(q3q4[i]);
+        
     }
 
     public override void Move() {
 
         Vector2 pos = transform.position;
 
-        for (int i = 0; i < corner.Count; i++) {
-            if (pos == corner[i]) {
-                targetCornor = ++targetCornor % corner.Count;
+        for (int i = 0; i < vertex.Count; i++) {
+            if (pos == vertex[i]) {
+                targetCornor = ++targetCornor % vertex.Count;
                 break;
             }
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, corner[targetCornor], Time.deltaTime * Speed);
+        transform.position = Vector2.MoveTowards(transform.position, vertex[targetCornor], Time.deltaTime * Speed);
     }
 
 }
