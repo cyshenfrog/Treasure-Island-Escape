45dfb35206f2b5e07edc15f2bf009e61ea63f7da
uno <st9007a@gmail.com>
2016-09-27 19:14:33 +0800

    refactor polygon path

--- Assets/Scripts/Monster/MonsterController/Controller/MController_8.cs
+++ Assets/Scripts/Monster/MonsterController/Controller/MController_8.cs
@@ -9,67 +9,93 @@ public class MController_8 : MController_2 {
     //path
     public int VertexCount;
     public int SideRange = 10;
+    public int SideRangeOffset = 3;
     public Vector2 Center = new Vector2(0, 0);
+    
 
     private List<Vector2> vertex = new List<Vector2>();
-    private int targetCornor = 0;
+    private int targetVertex;
 
     protected override void Awake() {
         base.Awake();
 
-        for (int i = 0; i < VertexCount; i++) {
-            int x = Random.Range(-SideRange/2, SideRange/2);
-            int y = Random.Range(-SideRange/2, SideRange/2);
+        //generate vertex
+        for (int i = 0; i < ((float)VertexCount) / 4 + 1; i++) {
+            int x = Random.Range(SideRange / 2 - SideRangeOffset, SideRange / 2 + SideRangeOffset);
+            int y = Random.Range(-SideRange / 2, SideRange / 2);
             vertex.Add(new Vector2(x, y));
         }
 
-        sortVertex();
+        for (int i = 0; i < ((float)VertexCount) / 4 + 1; i++)
+        {
+            int x = Random.Range(-SideRange / 2 - SideRangeOffset, -SideRange / 2 + SideRangeOffset);
+            int y = Random.Range(-SideRange / 2, SideRange / 2);
+            vertex.Add(new Vector2(x, y));
+        }
+
+        for (int i = 0; i < ((float)VertexCount) / 4 + 1; i++)
+        {
+            int x = Random.Range(-SideRange / 2, SideRange / 2);
+            int y = Random.Range(SideRange / 2 - SideRangeOffset, SideRange / 2 + SideRangeOffset);
+            vertex.Add(new Vector2(x, y));
+        }
+
+        for (int i = 0; i < ((float)VertexCount) / 4 + 1; i++)
+        {
+            int x = Random.Range(-SideRange / 2, SideRange / 2);
+            int y = Random.Range(-SideRange / 2 - SideRangeOffset, -SideRange / 2 + SideRangeOffset);
+            vertex.Add(new Vector2(x, y));
+        }
+
+        int c = vertex.Count;
+        for (int i = 0; i < c; i++)
+            vertex[i] += Center;
+
+        //according to arctan, sort vertex to circle path 
+        vertex.Sort((a, b) => { return Mathf.Atan2(a.y - Center.y, a.x - Center.x).CompareTo(Mathf.Atan2(b.y - Center.y, b.x - Center.x)); });
+        
     }
 
     protected override void Start() {
         State = MonsterState.MOVE;
+
+        int c = vertex.Count;
+        float dist = 1000000;
+        for (int i = 0; i < c; i++) {
+            if (Vector2.Distance(transform.position, vertex[i]) < dist) {
+                dist = Vector2.Distance(transform.position, vertex[i]);
+                targetVertex = i;
+            }
+        }
     }
 
     protected override void Update() {
         stateMachine();
     }
 
-    /// <summary>
-    /// sort vertex to polygon path
-    /// </summary>
-    private void sortVertex() {
-        List<Vector2> q1q2 = new List<Vector2>();
-        List<Vector2> q3q4 = new List<Vector2>();
-
-        int c = vertex.Count;
-        for (int i = 0; i < c; i++) {
-            if (vertex[i].y >= 0) q1q2.Add(vertex[i]);
-            else q3q4.Add(vertex[i]);
-        }
-
-        q1q2.Sort((x, y) => { return -x.x.CompareTo(y.x); });
-        q3q4.Sort((x, y) => { return x.x.CompareTo(y.x); });
-
-        vertex = q1q2;
-
-        c = q3q4.Count;
-        for (int i = 0; i < c; i++)
-            vertex.Add(q3q4[i]);
-        
-    }
-
     public override void Move() {
 
         Vector2 pos = transform.position;
 
+
         for (int i = 0; i < vertex.Count; i++) {
             if (pos == vertex[i]) {
-                targetCornor = ++targetCornor % vertex.Count;
+                targetVertex = ++targetVertex % vertex.Count;
                 break;
             }
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, vertex[targetCornor], Time.deltaTime * Speed);
+        moveToTarget();
+    }
+
+    private void moveToTarget() {
+        float atan = Mathf.Atan2(vertex[targetVertex].y - Center.y, vertex[targetVertex].x - Center.x) * Mathf.Rad2Deg;
+
+        Vector2 target = (atan > -45 && atan <= 45) || (atan > 135 || atan <= -135) ?
+            transform.position.y == vertex[targetVertex].y ? vertex[targetVertex] : new Vector2(transform.position.x, vertex[targetVertex].y) :
+            transform.position.x == vertex[targetVertex].x ? vertex[targetVertex] : new Vector2(vertex[targetVertex].x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * Speed);
+
     }
 
 }
