c96388435edbf6acc22278c85b41d4a45d370213
benwang0912 <benwang0912@gmail.com>
2016-10-26 21:10:32 +0800

    dropItem API for caramelYo

--- Assets/Scripts/Inventory/ItemManager.cs
+++ Assets/Scripts/Inventory/ItemManager.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ItemManager : MonoBehaviour {
+
+    static private Dictionary<string, GameObject> itemPrefab = new Dictionary<string, GameObject>();
 
     static public void dropItem(GameObject item, int count, Vector3 position) {
         GameObject dropItems = createDropItemsParent(position);
@@ -15,6 +18,44 @@ public class ItemManager : MonoBehaviour {
                 temp.tag = "Items";
             else if (count == 1)
                 temp.tag = "Item";
+        }
+    }
+
+    static public void dropItem(List<itemType> itemToDropList, List<int> amountList, List<Vector3> positionList) {
+        int i = 0;
+        foreach (itemType itemName in itemToDropList )
+        {
+            if (itemPrefab.ContainsKey(itemName.ToString()))
+            {
+                GameObject dropItems = createDropItemsParent(positionList[i]);
+                for (int amount = 0; amount < amountList[i]; ++amount)
+                {
+                    GameObject temp = Instantiate(itemPrefab[itemName.ToString()]);
+                    temp.transform.position = positionList[i];
+                    temp.transform.parent = dropItems.transform;
+                    if (amountList[i] > 1)
+                        temp.tag = "Items";
+                    else if (amountList[i] == 1)
+                        temp.tag = "Item";
+                }
+            }
+            else {
+                string path = string.Concat("Inventory/Items/", itemName.ToString());
+                itemPrefab.Add(itemName.ToString(), (Resources.Load(path, typeof(GameObject)) as GameObject));
+
+                GameObject dropItems = createDropItemsParent(positionList[i]);
+                for (int amount = 0; amount < amountList[i]; ++amount)
+                {
+                    GameObject temp = Instantiate(itemPrefab[itemName.ToString()]);
+                    temp.transform.position = positionList[i];
+                    temp.transform.parent = dropItems.transform;
+                    if (amountList[i] > 1)
+                        temp.tag = "Items";
+                    else if (amountList[i] == 1)
+                        temp.tag = "Item";
+                }
+            }
+            ++i;
         }
     }
 
